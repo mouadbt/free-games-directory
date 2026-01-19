2807c567d25038cba64fb37dbcd154b5ec46180f
mouadbt <160415648+mouadbt@users.noreply.github.com>
2026-01-19 23:32:18 +0100

    build web html strucutre

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const init = async () => {
+    console.log("Excute");
+};
+
+// load and execute and start script after page fully load
+document.addEventListener("DOMContentLoaded", () => {
+    init();
+});
